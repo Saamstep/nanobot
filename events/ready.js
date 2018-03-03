5ec5fee12fba0251fe7868f2b41dc1c78a89302b
Samstep.mac <Samstep2012@gmail.com>
2018-03-02 23:25:23 -0800

    fancy console! | events in folders

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,44 @@
 exports.run = (client, member, message) => {
 var config = require('../config.json');
 var colors = require('colors');
+var request = require('request');
 const guildNames = client.guilds.map(g => g.name).join("\n")
-console.log("+===========================================+".yellow)
-console.log(`${config.serverName}`.underline.cyan + " bot is online!\n".cyan + `\nConnected to:`.cyan + `\n${guildNames}`.italic.cyan);
-console.log("\n[IMPORTANT] KEEP THIS WINDOW OPEN FOR BOT TO STAY ONLINE".bold.red);
-console.log("+===========================================+".yellow)
+let bar = "+===========================================+"
+
+var url = "https://srhpyqt94yxb.statuspage.io/api/v2/status.json"
+
+request(url, function(err, response, body) {
+    if(err) {
+      console.log(err);
+      return message.reply('Error getting Mojang status.');
+    }
+    body = JSON.parse(body);
+    var indicator = body.status.indicator
+    var desc = body.status.description
+
+    if (body.status.description == "All Systems Operational")
+    {
+    console.log(bar.yellow)
+    console.log("[Discord Status] All Systems Operational\n".green);
+    } else {
+    console.log(bar.yellow)
+    console.log("[Discord Status] There seems to be an error within Discord. Double check http://www.status.discordapp.com/ \n".red);
+  }
+
+})
+
+
+setTimeout(() => {
+    console.log(`${config.serverName}`.underline.cyan + " bot is online!\n".cyan + `\nConnected to:`.cyan + `\n${guildNames}`.italic.cyan);
+    console.log("\n[IMPORTANT] KEEP THIS WINDOW OPEN FOR BOT TO STAY ONLINE".bold.red);
+    console.log(bar.yellow)
+    console.log("\nErrors will appear if you have debug mode enabled. Specific console log events will appear below as well.\n".italic.green);
+}, 500);
+
+
+
+
+
+
 
 }
