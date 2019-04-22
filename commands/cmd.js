3af0e4e481388a4fd157ae6a7e44816848e1d33c
Samstep.mac <Samstep2012@gmail.com>
2019-04-22 01:26:07 -0700

    [p]cmd broken

--- commands/cmd.js
+++ commands/cmd.js
@@ -1,37 +1,35 @@
 exports.run = (client, message, args) => {
-  const errorMod = require('../modules/errorMod.js');
-  const cM = require('../modules/consoleMod.js');
-  const log = require('../modules/logMod.js');
-  const config = require('../config.json');
-  const ConfigService = require('../config.js');
-  var Rcon = require('rcon');
-  var updatedport = Number(ConfigService.config.rconPort);
-  var conn = new Rcon(
-    `${ConfigService.config.mcIP}`,
-    updatedport,
-    `${ConfigService.config.rconPass}`
-  );
-  let cmd = args.join(' ');
+  // const errorMod = require('../modules/errorMod.js');
+  // const cM = require('../modules/consoleMod.js');
+  // const log = require('../modules/logMod.js');
+  // const config = require('../config.json');
+  // const ConfigService = require('../config.js');
+  // var Rcon = require('rcon');
+  // var updatedport = Number(ConfigService.config.rconPort);
+  // var conn = new Rcon(
+  //   `${ConfigService.config.mcIP}`,
+  //   updatedport,
+  //   `${ConfigService.config.rconPass}`
+  // );
+  // let cmd = args.join(' ');
 
-  let isAdmin = require('../modules/isAdmin.js');
-  if (isAdmin(message.author, message)) {
-    try {
-      conn
-        .connect()
-        .then(() => {
-          conn.send(cmd);
-          cM(`Command ${cmd} sent by ${message.author}`);
-          log('RCON', `/${cmd}`, 16774912, message);
-          message.channel.send('Command: `' + cmd + '` sent');
-        })
-        .then(() => {
-          console.log('Disconnecting...');
-          return conn.disconnect();
-        });
-    } catch {
-      console.error(error);
-    }
-  }
+  // let isAdmin = require('../modules/isAdmin.js');
+  // if (isAdmin(message.author, message)) {
+  //   async function commander() {
+  //     await conn.connect();
+  //     await conn.send(cmd);
+  //     await cM(`Command ${cmd} sent by ${message.author}`);
+  //     await log('RCON', `/${cmd}`, 16774912, message);
+  //     await message.channel.send('Command: `' + cmd + '` sent');
+  //     await conn.disconnect();
+  //   }
+  //   try {
+  //     commander();
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // }
+  message.reply(';)');
 };
 
 exports.description =
