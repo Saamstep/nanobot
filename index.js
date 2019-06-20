5b39fce55b2083b43fa93886d77f83b09682e7db
Samstep.mac <Samstep2012@gmail.com>
2019-06-19 23:53:09 -0700

    webhook integration over checkEmail() 85% done

--- index.js
+++ index.js
@@ -52,86 +52,101 @@ const defaultconfig = {
 };
 
 //Twitch Streamer Notifier
-const compare = new Set();
-async function twitch(message) {
+twitch = new Enmap({
+  name: 'twitch',
+  autoFetch: true,
+  fetchAll: false
+});
+
+async function twitch() {
   // List of streamers to get notifications for
 
-  ConfigService.config.streamers.forEach(async element => {
-    // Makes request
-    try {
-      client.console('Twitch | Getting streamer status: ' + element.magenta.dim);
-      const request = await fetch(`https://api.twitch.tv/kraken/streams?channel=${element}`, {
-        headers: {
-          'User-Agent': 'D.js-Bot-Dev',
-          'Client-ID': `${client.ConfigService.config.apis.twitch}`,
-          'content-type': 'application/json'
-        }
-      });
-      const body = await request.json();
-      if (body._total == 0) {
-        return client.console('Twitch | Found ' + element.magenta.dim + ' is not live');
-      }
-      if (!compare.has(element)) {
-        // Message formatter for the notificiations
-        const embed = {
-          description: '**' + body.streams[0].channel.status + '**',
-          url: 'http://twitch.tv/' + body.streams[0].channel.display_name,
-          color: 6684837,
-          footer: {
-            icon_url: 'https://cdn4.iconfinder.com/data/icons/social-media-circle-long-shadow/1024/long-10-512.png',
-            text: config.serverName + ' Bot'
-          },
-          thumbnail: {
-            url: body.streams[0].channel.logo
-          },
-          author: {
-            name: body.streams[0].channel.display_name + ' is live',
-            url: 'http://twitch.tv/' + body.streams[0].channel.display_name,
-            icon_url: 'https://cdn4.iconfinder.com/data/icons/social-media-circle-long-shadow/1024/long-10-512.png'
-          },
-          fields: [
-            {
-              name: 'Game',
-              value: body.streams[0].channel.game,
-              inline: true
-            },
-            {
-              name: 'Link',
-              value: 'http://twitch.tv/' + body.streams[0].channel.display_name,
-              inline: true
-            }
-          ]
-        };
-        // Add streamer name to a set
-        compare.add(element);
-
-        // Finds channel and sends msg to channel
-        // client.guilds.map(guild => {
-        //   if (guild.available) {
-        //     let channel = guild.channels.find(
-        //       channel => channel.name === `${client.ConfigService.config.channel.twitch}`
-        //     );
-        //     if (channel) {
-        //       channel.send(client.ConfigService.config.twitchMentionNotify, {
-        //         embed
-        //       });
-        //       client.console('Twitch | Found ' + element.magenta.dim + ' is live! Sending the announcement...');
-        //     }
-        //   }
-        // });
-        sendMessage(`${client.ConfigService.config.channel.twitch}`, client.ConfigService.config.twitchMentionNotify, {
-          embed
+  ConfigService.config.streamers
+    .forEach(async element => {
+      // Makes request
+      try {
+        client.console('Twitch | Getting streamer status: ' + element.magenta.dim);
+        const request = await fetch(`https://api.twitch.tv/kraken/streams?channel=${element}`, {
+          headers: {
+            'User-Agent': 'D.js-Bot-Dev',
+            'Client-ID': `${client.ConfigService.config.apis.twitch}`,
+            'content-type': 'application/json'
+          }
         });
-        client.console('Twitch | Found ' + element.magenta.dim + ' is live! Sending the announcement...');
-      } else if (compare.has(element) && body._total < 1) {
-        compare.delete(element);
+        const body = await request.json();
+        if (body._total == 0) {
+          return client.console('Twitch | Found ' + element.magenta.dim + ' is not live');
+        }
+        twitch.defer.then(() => {
+          if (!twitch.has(element)) {
+            // Message formatter for the notificiations
+            const embed = {
+              description: '**' + body.streams[0].channel.status + '**',
+              url: 'http://twitch.tv/' + body.streams[0].channel.display_name,
+              color: 6684837,
+              footer: {
+                icon_url: 'https://cdn4.iconfinder.com/data/icons/social-media-circle-long-shadow/1024/long-10-512.png',
+                text: config.serverName + ' Bot'
+              },
+              thumbnail: {
+                url: body.streams[0].channel.logo
+              },
+              author: {
+                name: body.streams[0].channel.display_name + ' is live',
+                url: 'http://twitch.tv/' + body.streams[0].channel.display_name,
+                icon_url: 'https://cdn4.iconfinder.com/data/icons/social-media-circle-long-shadow/1024/long-10-512.png'
+              },
+              fields: [
+                {
+                  name: 'Game',
+                  value: body.streams[0].channel.game,
+                  inline: true
+                },
+                {
+                  name: 'Link',
+                  value: 'http://twitch.tv/' + body.streams[0].channel.display_name,
+                  inline: true
+                }
+              ]
+            };
+
+            // Add streamer name to a set
+            twitch.set(element, true);
+
+            // Finds channel and sends msg to channel
+            // client.guilds.map(guild => {
+            //   if (guild.available) {
+            //     let channel = guild.channels.find(
+            //       channel => channel.name === `${client.ConfigService.config.channel.twitch}`
+            //     );tw
+            //     if (channel) {
+            //       channel.send(client.ConfigService.config.twitchMentionNotify, {
+            //         embed
+            //       });
+            //       client.console('Twitch | Found ' + element.magenta.dim + ' is live! Sending the announcement...');
+            //     }
+            //   }
+            // });
+            sendMessage(
+              `${client.ConfigService.config.channel.twitch}`,
+              client.ConfigService.config.twitchMentionNotify,
+              {
+                embed
+              }
+            );
+            client.console('Twitch | Found ' + element.magenta.dim + ' is live! Sending the announcement...');
+          } else if (twitch.has(element) && body._total < 1) {
+            twitch.delete(element);
+            return;
+          }
+        });
+        // });
+      } catch (e) {
+        client.console(e);
         return;
       }
-    } catch (e) {
-      client.console(e);
-      return;
-    }
-  });
+    })
+    .catch(console.error);
 }
 
 const owl = new Enmap({
@@ -339,7 +354,7 @@ function sendAuthEmail(email) {
           <head>
             <meta name="viewport" content="width=device-width">
             <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-            <title>Simple Transactional Email</title>
+            <title>Discord Server Invite</title>
             <style>
             /* -------------------------------------
                 INLINED WITH htmlemail.io/inline
@@ -710,11 +725,22 @@ async function youtubeNotifier() {
 }
 
 client.on('ready', ready => {
-  try {
-    setInterval(twitch, 180000);
-  } catch (e) {
-    client.console(e);
-  }
+  const http = require('http');
+  http
+    .createServer(function(req, res) {
+      console.log('STATUS: ' + res.statusCode);
+      req.on('data', function(chunk) {
+        client.console(chunk);
+      });
+      res.end();
+    })
+    .listen(3000);
+
+  // try {
+  //   setInterval(twitch, 1000);
+  // } catch (e) {
+  //   client.console('Twitch: ' + e);
+  // }
   try {
     setInterval(youtubeNotifier, 300000);
   } catch (e) {
