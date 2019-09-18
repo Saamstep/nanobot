527815bf2ce0656eeb2881ee1828e818a40749ac
Samstep.mac <Samstep2012@gmail.com>
2019-09-17 22:50:03 -0700

    isOwner bug fixed

--- modules/isOwner.js
+++ modules/isOwner.js
@@ -1,6 +1,6 @@
 module.exports = function isOwner(message, msg, client) {
   let error = require('../modules/errorMod.js');
-  if (message.author.id == `${client.settings.get(`${message.guild.id}`, 'ownerid')}`) {
+  if (message.author.id == `${client.ConfigService.config.ownerid}`) {
     return true;
   } else {
     if (msg == true) {
