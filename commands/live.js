d456f38cbf529db14bcc8978426609ab2aea6178
Samstep.mac <Samstep2012@gmail.com>
2019-10-13 15:18:14 -0700

    live channel notifier with red circle

---
+++ commands/live.js
@@ -0,0 +1,13 @@
+exports.run = async (client, message, args) => {
+  if (!message.member.voiceChannel) return client.error('Join a voice channel!', message);
+  const voiceChannels = message.guild.channels.filter(channel => channel.type === 'voice');
+  const vc = voiceChannels.find(n => n.name == `${message.member.voiceChannel.name}`);
+
+  if (vc.name.includes('🔴')) {
+    let newName = vc.name.replace('🔴', '');
+    vc.edit({ name: `${newName}` });
+  } else {
+    let current = vc.name;
+    vc.edit({ name: `🔴 ${current}` });
+  }
+};
