9595a01edf8a1ad18ce985899b14a30052ff0cc2
Samstep.mac <Samstep2012@gmail.com>
2019-11-03 01:32:27 -0700

    closer to twitchhook being done

--- commands/twitch.js
+++ commands/twitch.js
@@ -2,12 +2,12 @@ exports.run = async (client, message, args, veriEnmap, cc) => {
   const fetch = require('node-fetch');
   const date = require('dateformat');
   async function getUser(name) {
-    const req = await fetch(`https://api.twitch.tv/helix/streams?user_login=${name}`, {
+    const req = await fetch(`https://api.twitch.tv/helix/users?login=${name}`, {
       headers: {
         'Client-ID': `${client.ConfigService.config.apis.twitch}`
       }
     });
-    const userData = req.json();
+    const userData = await req.json();
     return await userData;
   }
   async function isLive(name) {
@@ -19,12 +19,60 @@ exports.run = async (client, message, args, veriEnmap, cc) => {
     const streamData = await request.json();
     return await streamData;
   }
+  async function getGame(game) {
+    const re = await fetch(`https://api.twitch.tv/helix/games?id=${game}`, {
+      headers: {
+        'Client-ID': `${client.ConfigService.config.apis.twitch}`
+      }
+    });
+    const game = await re.json();
+    return await game;
+  }
   switch (args[0]) {
     case 'add':
       break;
     case 'remove':
       break;
     case 'list':
+      break;
+    case 'user':
+      if (!args[1]) return client.error('You must provide a Twitch username!', message);
+      let user = await getUser(`${args[1]}`);
+      if (user.data[0].length < 1) return client.error('That streamer does not exist!', message);
+      const embed = {
+        title: `${user.data[0].display_name} on Twitch`,
+        // description: `${user.data[0].title}`,
+        url: `https://twitch.tv/${user.data[0].login}`,
+        color: 9442302,
+        footer: {
+          icon_url: client.user.avatarURL,
+          text: client.user.username + ' - Twitch'
+        },
+        fields: [
+          {
+            name: 'Type',
+            value: `${user.data[0].broadcaster_type.toUpperCase() || 'N/A'}`,
+            inline: true
+          },
+          {
+            name: 'Views',
+            value: `${user.data[0].view_count || '0'}`,
+            inline: true
+          },
+          {
+            name: 'Description',
+            value: `${user.data[0].description || 'No description set.'}`,
+            inline: false
+          }
+        ],
+        thumbnail: {
+          url: `${user.data[0].profile_image_url}`
+        },
+        image: {
+          url: `${user.data[0].offline_image_url}`
+        }
+      };
+      message.channel.send({ embed });
       break;
     case 'live':
       if (!args[1]) return client.error('You must provide a Twitch username!', message);
--- services/twitchHook.js
+++ services/twitchHook.js
@@ -1,5 +1,16 @@
-exports.run = (client, dupe, veriEnmap, sendMessage) => {
+exports.run = async (client, dupe, veriEnmap, sendMessage) => {
   const http = require('http');
+  const date = require('dateformat');
+  const fetch = require('node-fetch');
+  async function getUser(name) {
+    const req = await fetch(`https://api.twitch.tv/helix/users?login=${name}`, {
+      headers: {
+        'Client-ID': `${client.ConfigService.config.apis.twitch}`
+      }
+    });
+    const userData = req.json();
+    return await userData;
+  }
   http
     .createServer(function(req, res) {
       let body = '';
@@ -7,11 +18,48 @@ exports.run = (client, dupe, veriEnmap, sendMessage) => {
       req.on('data', function(chunk) {
         body += chunk;
       });
-      req.on('end', function() {
+      req.on('end', async function() {
         if (body) {
+          // let hash = crypto
+          //   .createHmac(incoming[0], secret)
+          //   .update(JSON.stringify(req.body))
+          //   .digest('hex');
+          // if (incoming == undefined) return client.console('TwitchHook | Unauthorized request!');
           client.console('TwitchHook Found');
-          let data = JSON.parse(body);
-          console.log(data);
+          let data = JSON.parse(body).data[0];
+          if (data.type == 'live') {
+            const user = await getUser(data.user_name);
+            let pfp = user.data[0].profile_image_url;
+            const embed = {
+              title: `${user.data[0].display_name} is live on Twitch!`,
+              description: `${data.title}`,
+              url: `https://twitch.tv/${data.user_name}`,
+              color: 9442302,
+              footer: {
+                icon_url: client.user.avatarURL,
+                text: client.user.username + ' - Twitch'
+              },
+              fields: [
+                {
+                  name: 'Viewers',
+                  value: `${data.viewer_count}`,
+                  inline: true
+                },
+                {
+                  name: 'Started on',
+                  value: `${date(data.started_at, 'm/d/yy @ hh:MM TT')}`,
+                  inline: true
+                }
+              ],
+              image: {
+                url: `${data.thumbnail_url.replace(`{width}`, '1920').replace(`{height}`, '1080')}`
+              },
+              thumbnail: {
+                url: `${pfp}`
+              }
+            };
+            sendMessage('testing', { embed });
+          }
         }
       });
       res.end('<h1>TwitchLive</h1>');
