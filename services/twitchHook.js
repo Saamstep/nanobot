fc6d6056fbe2af5b32de2ad5ca327eaf50defdd1
Samstep.mac <Samstep2012@gmail.com>
2019-11-04 13:13:04 -0800

    challenge send

--- services/twitchHook.js
+++ services/twitchHook.js
@@ -34,47 +34,54 @@ exports.run = async (client, dupe, veriEnmap, sendMessage) => {
           //   .update(JSON.stringify(req.body))
           //   .digest('hex');
           // if (incoming == undefined) return client.console('TwitchHook | Unauthorized request!');
-
-          client.console('TwitchHook Found');
-          let data = JSON.parse(body).data[0];
-          if (data.type == 'live') {
-            const user = await getUser(data.user_name);
-            const game = await getGame(data.game_id);
-            let pfp = user.data[0].profile_image_url;
-            const embed = {
-              title: `${user.data[0].display_name} is live on Twitch!`,
-              description: `${data.title}`,
-              url: `https://twitch.tv/${data.user_name}`,
-              color: 9442302,
-              footer: {
-                icon_url: client.user.avatarURL,
-                text: client.user.username + ' - Twitch'
-              },
-              fields: [
-                {
-                  name: 'Viewers',
-                  value: `${data.viewer_count}`,
-                  inline: true
+          var parts = url.parse(request.url, true);
+          var challenge = parts.query['hub.challenge'];
+          if (challenge == undefined) {
+            client.console('TwitchHook Found');
+            let data = JSON.parse(body).data[0];
+            if (data.type == 'live') {
+              const user = await getUser(data.user_name);
+              const game = await getGame(data.game_id);
+              let pfp = user.data[0].profile_image_url;
+              const embed = {
+                title: `${user.data[0].display_name} is live on Twitch!`,
+                description: `${data.title}`,
+                url: `https://twitch.tv/${data.user_name}`,
+                color: 9442302,
+                footer: {
+                  icon_url: client.user.avatarURL,
+                  text: client.user.username + ' - Twitch'
                 },
-                {
-                  name: 'Game',
-                  value: `${game.data[0].name}`,
-                  inline: true
+                fields: [
+                  {
+                    name: 'Viewers',
+                    value: `${data.viewer_count}`,
+                    inline: true
+                  },
+                  {
+                    name: 'Game',
+                    value: `${game.data[0].name}`,
+                    inline: true
+                  },
+                  {
+                    name: 'Started on',
+                    value: `${date(data.started_at, 'm/d/yy @ hh:MM TT')}`,
+                    inline: false
+                  }
+                ],
+                image: {
+                  url: `${data.thumbnail_url.replace(`{width}`, '1920').replace(`{height}`, '1080')}`
                 },
-                {
-                  name: 'Started on',
-                  value: `${date(data.started_at, 'm/d/yy @ hh:MM TT')}`,
-                  inline: false
+                thumbnail: {
+                  url: `${pfp}`
                 }
-              ],
-              image: {
-                url: `${data.thumbnail_url.replace(`{width}`, '1920').replace(`{height}`, '1080')}`
-              },
-              thumbnail: {
-                url: `${pfp}`
-              }
-            };
-            sendMessage(client.ConfigService.config.channel.twitch, { embed });
+              };
+              sendMessage(client.ConfigService.config.channel.twitch, { embed });
+            }
+          } else {
+            res.writeHead(200);
+            console.log(`challenge: ${challenge}`);
+            res.send(challenge);
           }
         }
       });
@@ -82,22 +89,4 @@ exports.run = async (client, dupe, veriEnmap, sendMessage) => {
     })
     .listen(9697);
   const url = require('url');
-  const requestHandler = (request, response) => {
-    var parts = url.parse(request.url, true);
-    var challenge = parts.query['hub.challenge'];
-    if (challenge == undefined) {
-      return;
-    } else {
-      response.writeHead(200);
-    }
-    console.log(`challenge: ${challenge}`);
-    response.end(challenge);
-  };
-
-  http.createServer(requestHandler).listen(9696, err => {
-    if (err) {
-      return console.log('something bad happened', err);
-    }
-    console.log(`server is listening on 9696`);
-  });
 };
