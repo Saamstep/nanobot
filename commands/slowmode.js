55de6381c77ae8687f5d948ad98c0d8b57332325
Samstep.mac <Samstep2012@gmail.com>
2019-11-25 23:04:20 -0800

    slowmode seconds bug

--- commands/slowmode.js
+++ commands/slowmode.js
@@ -1,7 +1,7 @@
 exports.run = (client, message, args) => {
-  let time = args[1] + ' seconds' || 'N/A';
+  let time = args[1] || "//";
   const embed = {
-    description: 'This channel is now in slowmode',
+    description: "This channel is now in slowmode",
     color: 14792508,
     timestamp: Date.now(),
     footer: {
@@ -9,17 +9,17 @@ exports.run = (client, message, args) => {
       text: client.username
     },
     author: {
-      name: 'Slowmode Enabled'
+      name: "Slowmode Enabled"
     },
     fields: [
       {
-        name: 'Speed',
+        name: "Speed",
         value: `${args[0]} seconds`,
         inline: true
       },
       {
-        name: 'Time',
-        value: time,
+        name: "Time",
+        value: time + " seconds",
         inline: true
       }
     ]
@@ -27,31 +27,24 @@ exports.run = (client, message, args) => {
 
   if (client.isMod(message.author, message, client)) {
     if (!args[0] && !args[1]) {
-      return message.channel.send(
-        `\`\`\`${client.ConfigService.config.prefix}slowmode [speed] [time (optional)]\`\`\``
-      );
+      return message.channel.send(`\`\`\`${client.ConfigService.config.prefix}slowmode [speed] [time (optional)]\`\`\``);
     }
     if (args[0] && args[1]) {
-      message.channel
-        .setRateLimitPerUser(args[0], `Rate limit set by ${message.author.username}#${message.author.discriminator}`)
-        .then(r => {
-          setTimeout(function() {
-            message.channel.setRateLimitPerUser(0, `Disabled automatically.`);
-          }, args[1] * 1000);
-        });
+      message.channel.setRateLimitPerUser(args[0], `Rate limit set by ${message.author.username}#${message.author.discriminator}`).then(r => {
+        setTimeout(function() {
+          message.channel.setRateLimitPerUser(0, `Disabled automatically.`);
+        }, args[1] * 1000);
+      });
       message.channel.send({ embed });
     } else {
-      message.channel.setRateLimitPerUser(
-        args[0],
-        `Rate limit set by ${message.author.username}#${message.author.discriminator}`
-      );
+      message.channel.setRateLimitPerUser(args[0], `Rate limit set by ${message.author.username}#${message.author.discriminator}`);
       message.channel.send({ embed });
     }
   }
 };
 exports.cmd = {
   enabled: true,
-  category: 'Moderation',
+  category: "Moderation",
   level: 1,
-  description: 'Allows mods to enable Discord slowmode via command'
+  description: "Allows mods to enable Discord slowmode via command"
 };
