ddf6326666563387cb9db6d169d705f71e5d0c3f
Samstep.mac <Samstep2012@gmail.com>
2019-12-12 12:33:09 -0800

    properties now updatable with dots

--- commands/settings.js
+++ commands/settings.js
@@ -5,9 +5,9 @@ exports.run = async (client, message, args, veriEnmap, cc) => {
 
   function hasNoDisabled(input) {
     if (input == "token" || input == "ownerid" || input.includes("apis")) {
-      return false;
-    } else {
       return true;
+    } else {
+      return false;
     }
   }
   function push(out) {
@@ -20,14 +20,31 @@ exports.run = async (client, message, args, veriEnmap, cc) => {
     case "remove":
       break;
     case "edit":
-      if (hasNoDisabled(args[1]) && !args[1].includes(",")) {
+      //make sure we dont edit something that shouldn't be
+      if (hasNoDisabled(args[1])) return client.error("You cannot edit that!", message);
+      //dot prop
+      if (args[1].includes(".")) {
+        let copy = args[1].split(".");
+        if(copy.length == 2) {
+        if (data[`${copy[0]}`][`${copy[1]}`] == undefined) return client.error(`The property \`${args[1]}\` does not exist!`, message);
+        if(Array.isArray(data[`${copy[0]}`][`${copy[1]}`]))
+        data[`${copy[0]}`][`${copy[1]}`] = args[2];
+        }
+        //TODO integrate double dot accessor
+        push(data);
+      } else {
+        //array prop
+        if (Array.isArray(data[args[1]])) return console.log("hi") {
+
+        }
+        //normal prop
         data[args[1]] = args[2];
         push(data);
       }
-      if (hasNoDisabled(args[1]) && args[1].includes(".")) {
-        let copy = args[1].split(".");
-        data.copy[0].copy[1];
-      }
+
+      //array
+      // if()
+      message.channel.send(`Updated property \`${args[1]}\` to \`${args[2]}\``);
       break;
     default:
       let msg = `${client.user.username} Settings | Change these config properties (except owner ID lol)\n-------\n<General>\n`;
