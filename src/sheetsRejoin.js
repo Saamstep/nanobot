dac57809c104a03f9119c2133d08be468e1ef687
Samstep.mac <Samstep2012@gmail.com>
2020-03-29 00:10:52 -0700

    error loading creds for sheetRejoin

--- src/sheetsRejoin.js
+++ src/sheetsRejoin.js
@@ -10,7 +10,7 @@ exports.run = (client, sendMessage, member) => {
   const TOKEN_PATH = "../token.json";
 
   // Load client secrets from a local file.
-  fs.readFile("credentials.json", (err, content) => {
+  fs.readFile("../credentials.json", (err, content) => {
     if (err) return console.log("Error loading client secret file:", err);
     // Authorize a client with credentials, then call the Google Sheets API.
     authorize(JSON.parse(content), findDiscord);
