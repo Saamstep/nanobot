e5572595b6cbbe5b9b8a5f9dddd4ff933dd26dea
Samstep.mac <Samstep2012@gmail.com>
2020-04-03 18:27:25 -0700

    updated to support bungee

--- services/mcAuth.js
+++ services/mcAuth.js
@@ -1,14 +1,15 @@
-exports.run = (client, dupe, sendMessage) => {
+exports.run = async (client, dupe, sendMessage) => {
   const fs = require("fs");
+  const fetch = require("node-fetch");
   const express = require("express");
   var app = express();
   //define guild from ID in config
   const guild = client.guilds.get(client.ConfigService.config.guild);
   app.use(express.json());
   app.listen(5000, () => {
-    client.console("Listening at port 4000", "info", "Verification");
+    client.console("Listening at port 5000", "info", "mcAuth");
   });
-  app.post("/newuser", (req, res) => {
+  app.post("/newuser", async (req, res) => {
     //some basic auth
     if (req.headers["key"] != client.ConfigService.config.apis.discordJoin) {
       //api key checker
@@ -17,12 +18,27 @@ exports.run = (client, dupe, sendMessage) => {
     } else if (Object.keys(req.body).length > 0) {
       res.status(200).send({ status: "Successful" });
       //DO STUFF HERE NOW
-      client.rcon.send(`whitelist add ${req.body.username}`);
-      let member = guild.members.find(m => m.user.username == req.body.discord.split("#")[0] && m.user.discriminator == req.body.discord.split("#")[1]);
-      member.addRole(guild.roles.find(r => r.name == "Minecraft Server Player"));
-      member.send(`> You have been whitelisted to our Minecraft Server with the account **${req.body.username}**\n> Please make sure to read the rules of the server at https://vchsesports.net/mcserver\n> The IP is ||\`mc.vchsesports.net:25580\`||`);
-      sendMessage("minecraft-server-connected", `<@${member.user.id}> was whitelisted. Welcome to the server **${req.body.username}**`);
-      // client.rcon.send(`whitelist add ${req.body.}`);
+      try {
+        const usr = await fetch(`https://api.mojang.com/users/profiles/minecraft/${req.body.username}`);
+        const user = await usr.json();
+        console.log(user);
+      } catch (e) {
+        if (e.type == "invalid-json") {
+          return sendMessage(client.ConfigService.config.channel.log, `> __AutoWhitelist__\n> The AutoWhitelister failed for username **${req.body.username}**. This username does not exist.`);
+        } else {
+          client.console(e, "error", "mcAuth");
+        }
+      }
+      // await require("./rcon.js").run(client, "");
+      await client.rcon.send(`whitelist add ${req.body.username}`);
+      // await require("./rcon.js").run(client, "dc");
+      console.log(req.body.discord);
+      if (req.body.discord.includes("#")) {
+        let member = guild.members.find((m) => m.user.username == req.body.discord.split("#")[0] && m.user.discriminator == req.body.discord.split("#")[1]);
+        member.addRole(guild.roles.find((r) => r.name == "Minecraft Server Player"));
+        member.send(`> You have been whitelisted to our Minecraft Server with the account **${req.body.username}**\n> Please make sure to read the rules of the server at https://vchsesports.net/mcserver\n> The IP is ||\`mc.vchsesports.net\`||`);
+        sendMessage("minecraft-server-connected", `<@${member.user.id}> was whitelisted. Welcome to the server **${req.body.username}**`);
+      }
     } else {
       res.status(400).send({ error: "No body" });
     }
--- services/rcon.js
+++ services/rcon.js
@@ -1,4 +1,4 @@
-exports.run = (client, dupe, sendMessage) => {
+exports.run = (client) => {
   var Rcon = require("rcon");
 
   if (Object.values(client.ConfigService.config.smp.rcon)[0] == "" || Object.values(client.ConfigService.config.smp.rcon)[1] == null) {
@@ -8,13 +8,13 @@ exports.run = (client, dupe, sendMessage) => {
 
   var conn = new Rcon(client.ConfigService.config.smp.ip, client.ConfigService.config.smp.rcon.port, client.ConfigService.config.smp.rcon.pass);
   conn
-    .on("auth", function() {
+    .on("auth", function () {
       client.console(`Authorized and connected to server ${client.ConfigService.config.smp.ip}`, "info", "RCON");
     })
-    .on("error", function(error) {
+    .on("error", function (error) {
       client.console(`Server ${client.ConfigService.config.smp.ip} :: ${error}`, "error", "RCON");
     })
-    .on("end", function() {
+    .on("end", function () {
       client.console(`Socket closed with server ${client.ConfigService.config.smp.ip}`, "warn", "RCON");
       if (client.ConfigService.config.smp.rcon.autoRestart) {
         conn.connect();
